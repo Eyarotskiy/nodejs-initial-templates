81ba6ddc5c19477e102c9e412240d90d65c04bc0
Eyarotskiy <eyarotskiy94@gmail.com>
2020-05-09 22:44:50 +0200

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
 
@@ -8,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
+          <h1 className="App-title">Node + React template</h1>
         </header>
         <Customers />
       </div>
